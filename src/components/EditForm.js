c955b04af4f458c27c1565a5751f4df186f7946f
MaheshZilpe <mzilpe22@gmail.com>
2021-10-18 13:31:38 +0530

    fd

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 
 const EditForm = (props) => {
 	const { editData, setTableData, tableData, setEditData } = props;
